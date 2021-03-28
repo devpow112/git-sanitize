9b3245eb1098b5568c1db90344d7b0398d3308d8
Devon Powell <devon.f.powell@gmail.com>
2021-03-27 21:37:16 -0400

    fix: execution

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import { sync as commandExistsSync } from 'command-exists';
 import { executeGit } from './execute-git';
 
 if (commandExistsSync('git')) {
-  const submoduleForEach = ['submodule', 'foreach', '--recursive'];
-
-  try {
-    executeGit(['clean', '-xfdf']);
-    executeGit(submoduleForEach.concat(['git', 'clean', '-xfdf']));
-    executeGit(['reset', '--hard']);
-    executeGit(submoduleForEach.concat(['git', 'reset', '--hard']));
-    executeGit(['submodule', 'update', '--recursive', '--init']);
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-} else {
   console.error('git is not available');
   process.exit(2);
+}
+
+const submoduleForEach = ['submodule', 'foreach', '--recursive'];
+
+try {
+  executeGit(['clean', '-xfdf']);
+  executeGit(submoduleForEach.concat(['git', 'clean', '-xfdf']));
+  executeGit(['reset', '--hard']);
+  executeGit(submoduleForEach.concat(['git', 'reset', '--hard']));
+  executeGit(['submodule', 'update', '--recursive', '--init']);
+} catch (err) {
+  console.error(err.message);
+  process.exit(1);
 }
